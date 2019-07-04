4b8faabe4f9a8deca1ce23e34db567d00af7e8b1
Valerie Manuel <valerie.manuel11@gmail.com>
2019-07-04 19:35:19 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,8 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-  def initialize(brand)
-    @brand = brand
+  def initialize=(brand)
+    @brand=brand
     BRAND<< brand
   end
 
